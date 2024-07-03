6e96cba9953df4337756558fc3b1524eadee46da
Wendimu Sitotaw <wendimusit@gmail.com>
2024-07-03 21:08:04 +0300

    add routing to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
 import './App.css'
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Component from './components/Component'
+import Login from './components/Login';
+import Signup from './components/Signup';
 function App() {
- return(
-  <>
-  <Component/>
-  </>
- )
+    return (
+        <Routes>
+            <Route path="/" element={<Component />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/singup" element={<Signup />}></Route>
+        </Routes>
+    )
 }
 
 export default App
